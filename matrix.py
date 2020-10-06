e5e1d3b7aaf803508bea6a4a438c1f20fa04d8d2
dungpt3003 <dungpt3003@gmail.com>
2020-10-06 10:46:06 +0100

    Add functions to generate matrices B and C

--- matrix.py
+++ matrix.py
@@ -2,11 +2,13 @@
 # This files contains functions to construct matrices 
 import numpy as np
 
+# Load normal matrix from file
 def loadMatrix(aFile):
     with open(aFile, "r") as f:
         A = np.loadtxt(f, delimiter=",")
         return A
 
+# Load values from file and create a diagonal matrix
 def loadDiagonalMatrix(aFile):
     with open(aFile, "r") as f:
         temp = np.loadtxt(f,delimiter=",")
@@ -14,6 +16,16 @@ def loadDiagonalMatrix(aFile):
         for i in range(0,temp.shape[0]):
             T[i,i] = temp[i]
         return T
+
+# Generate matrix matrix B as the Kronecker product of A and the identity matrix size N
+def generateMatrixB(A, N):
+    return np.kron(A, np.eye(N))
+
+# Generate matrix C as the Kronecker product of two diagonal matrices (T and diagN)
+def generateMatrixC(T, N):
+    diagN = np.eye(N)
+    return np.kron(T, diagN)
+
 
 if __name__ == '__main__':
     A = loadMatrix("data/a.csv")
