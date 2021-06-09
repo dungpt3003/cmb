842789e2d0f021caec755b75ab4990c4e3487f1f
Dung Pham <dungpt@Dungs-MacBook-Pro.local>
2021-06-09 16:15:46 +0100

    Generate random source s by matrix B instead of matrix A

--- evaluation.py
+++ evaluation.py
@@ -7,29 +7,36 @@ from matrix import *
 from utils import *
 from conjugate_solution import ConjugateSolution
 
+def generate_random_sources(n, m, h, mean_s, sd_s, printable=True):
+    """Generate random source s and source vector y
+    Parameters
+    ----------
+    n : int
+        number of frequencies
+    m : int
+        number of diffuse sources
+    h : int
+        HEALPix level
+    mean_s: float
+        Mean value of the random source s
+    sd_s: float
+        Standard deviation value of the random source s
+    """
+    A = calculate_matrix_A(n, m)
+    N = calculate_N_from_level(h)
+    B = generate_matrix_B(A, N)
+    T = load_diagonal_matrix("data/t.csv")
+    C = generate_matrix_C(T, N)
+    s = np.random.normal(mean_s, sd_s, m*N)
 
-def generate_random_sources(h):
-    base_patch = 4 ** h
-    S = np.random.rand(4, base_patch)
-    return S
+    mean_y = np.matmul(B, s)
+    sd = np.array([1/np.sqrt(x) for x in np.diag(C)])
+    y = np.random.normal(mean_y, sd, mean_y.shape[0])
+    if printable:
+        np.savetxt('output/random_y.txt', [y], delimiter=',', fmt='%0.20f')
+        np.savetxt('output/random_s.txt', [s], delimiter=',', fmt='%0.20f')
 
-def generate_sources(m, n, h):
-    repetition = 12
-    base_patch = 4 ** h
-    N = repetition * base_patch
-    A = calculate_matrix_A(m, n)
-    y = np.zeros(0)
-    S_ = np.zeros(0)
-    mean = np.zeros(m)
-    T = load_diagonal_matrix_inverse("data/t.csv")
-    for i in range(repetition):
-        S = generate_random_sources(h)
-        S_ = np.append(S_, S)
-        for j in range(base_patch):
-            y_temp = np.matmul(A, S[:, j]) + np.random.multivariate_normal(mean, T)
-            y = np.append(y, y_temp)
-    return y, S_
-
+    return s, y
     
 if __name__ == '__main__':
     m = 9
@@ -38,18 +45,22 @@ if __name__ == '__main__':
     sourcefilet = "data/t.csv"
     tnum = 4
 
-    for i in range(1,2):
+    generate_random_sources(9, 4, 3, 1, 0.1, True)
+    # for i in range(1,10):
         
-        print("-------------lvl:{0}, N:{1}------------".format(i, calculate_N_from_level(i)))
-        #y = 5 * np.random.randint(0,10,(m * calculate_N_from_level(i), 1))
-        #iterate method
-        y, S = generate_sources(9, 4, i)
-        start = time.time()
-        ite_s = ConjugateSolution(sourcefilea,sourcefilet,m,n,i,y,0.001,tnum)
-        ite_x = ite_s.findSolution()
-        end = time.time()
-        print("iterate method took:{0}s".format(end - start))
-        sys.stdout.flush()
+    #     print("-------------lvl:{0}, N:{1}------------".format(i, calculate_N_from_level(i)))
+    #     #y = 5 * np.random.randint(0,10,(m * calculate_N_from_level(i), 1))
+    #     #iterate method
+    #     y, S = generate_sources(9, 4, i)
+    #     start = time.time()
+    #     ite_s = ConjugateSolution(sourcefilea,sourcefilet,m,n,i,y,0.001,tnum)
+    #     ite_x = ite_s.findSolution()
+    #     #pretty_print_matrix_S(ite_x, "x.txt")
+    #     with open("x_.npy", "wb") as f:
+    #         np.save(f, ite_x)
+    #     end = time.time()
+    #     print("iterate method took:{0}s".format(end - start))
+    #     sys.stdout.flush()
 
         #std solution
         #start = time.time()
